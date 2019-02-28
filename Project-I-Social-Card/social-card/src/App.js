88e049ec7f40e035093e422739da394a07ae5184
GregoryUrban <turknben@gmail.com>
2019-02-27 23:13:59 -0500

    Added Footer component, content and css. Even used some state values!

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -2,9 +2,7 @@ import React from 'react';
 import './App.css';
 import HeaderContainer from './components/HeaderComponents/HeaderContainer';
 import CardContainer from './components/CardComponents/CardContainer';
-// import CardContainer from './components/CardComponents/CardContainer';
-// import Footer from './components/FooterComponents/Footer';
-
+import Footer from './components/FooterComponents/Footer';
 
 
 
@@ -13,7 +11,7 @@ const App = () => {
     <div>
     <HeaderContainer />
     <CardContainer />
-    {/* <Footer /> */}
+    <Footer />
   </div>
   );
 };
