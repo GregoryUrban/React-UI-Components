094a4730f1c425cf7907c529a0e25ebb8ec63eef
GregoryUrban <turknben@gmail.com>
2019-02-27 22:52:01 -0500

    href link is clicked when any area in card-container is clicked

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -4,10 +4,13 @@ import CardContent from "./CardContent";
 import "./Card.css";
 
 const CardContainer = () => (
+    <a href='https://reactjs.org/' target = '_blank'>
+    
     <div className='card-container'>
         <CardBanner />
         <CardContent />
     </div>
+    </a>
 );
 
 export default CardContainer;
