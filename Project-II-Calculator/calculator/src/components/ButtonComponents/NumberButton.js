ab89c0a3607d72361dadb37c97f2a77912435ee7
GregoryUrban <turknben@gmail.com>
2019-02-28 23:44:48 -0500

    Calculator - Initial Commit

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = () => {
+    return (
+      <div>
+        I am a number button.
+      </div>
+    );
+  };
+  
+  export default NumberButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,12 @@
 import React from 'react';
 import './Display.css';
+
+const CalculatorDisplay = () => {
+    return (
+    <div> 
+    I am Calculator Display.
+    </div>
+    );
+  };
+  
+  export default CalculatorDisplay;
